60b2a4ec39785127d721843fd50bce9e5187c1b7
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-05 13:42:32 +0200

    rebuild after addind key

--- dist/ClipMaker.js
+++ dist/ClipMaker.js
@@ -7,10 +7,10 @@ const ClipMaker = ({ props }) => {
     const clipMakerProps = JSON.parse(props);
     const clips = clipMakerProps.clips;
     let from = 0;
-    return (jsx_runtime_1.jsx(jsx_runtime_1.Fragment, { children: clips.map(clip => {
+    return (jsx_runtime_1.jsx(jsx_runtime_1.Fragment, { children: clips.map((clip, clipIndex) => {
             const clipStart = clip.from;
             const durationInFrames = clip.durationInFrames;
-            const sequence = jsx_runtime_1.jsx(remotion_1.Sequence, Object.assign({ from: from, durationInFrames: durationInFrames }, { children: jsx_runtime_1.jsx(remotion_1.Video, { src: clip.video, startFrom: clipStart, endAt: clipStart + durationInFrames }, void 0) }), void 0);
+            const sequence = jsx_runtime_1.jsx(remotion_1.Sequence, Object.assign({ from: from, durationInFrames: durationInFrames }, { children: jsx_runtime_1.jsx(remotion_1.Video, { src: clip.video, startFrom: clipStart, endAt: clipStart + durationInFrames }, void 0) }), clipIndex);
             from += durationInFrames;
             return sequence;
         }) }, void 0));
