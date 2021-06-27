9ef902a14437947e384138e819df0319629c0146
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2021-06-27 20:01:25 +0200

    Update Video.tsx

--- src/Video.tsx
+++ src/Video.tsx
@@ -9,10 +9,10 @@ export const RemotionVideo: React.FC = () => {
 			<Composition
 				id="ClipMaker"
 				component={ClipMaker}
-				durationInFrames={durationInFrames ?? 600}
-				fps={fps ?? 60}
-				width={width ?? 1080}
-				height={height ?? 1920}
+				durationInFrames={parseInt(durationInFrames) ?? 600}
+				fps={parseInt(fps) ?? 60}
+				width={parseInt(width) ?? 1080}
+				height={parseInt(height) ?? 1920}
 				defaultProps={{
 					props: "{\"clips\":[{\"video\":\"https://api.spinner.ggio.fr/cache/115.mp4\",\"from\":0,\"durationInFrames\":600,\"moves\":{\"0\":{\"offset\":0,\"transition\":\"none\"},\"144\":{\"offset\":25.474268292682925,\"transition\":\"smooth\"},\"268\":{\"offset\":68.29268292682927,\"transition\":\"none\"},\"600\":{\"offset\":0,\"transition\":\"none\"}}}],\"texts\":[{\"content\":\"Je suis un joli text\",\"from\":0,\"durationInFrames\":600,\"height\":7,\"color\": \"#fff\",\"backgroundColor\":\"rgb(255,165,0)\",\"backgroundColorOpacity\":0.7,\"leftOffset\":20,\"rightOffset\":20,\"topOffset\":40}]}"
 				}}
